844566c80f50ad874e4a53438cedd16a2d2f2f81
Jason Pendley <Jason.R.Pendley-1@ou.edu>
2020-08-08 17:26:57 -0500

    My Simple To-Do List

---
+++ Task/NewWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace NewWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Task/NewWeb/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Dictionary<int, string> TaskList = new Dictionary<int, string>();
+            TaskList.Add(1, "Clean my room");
+            TaskList.Add(2, "Read a book");
+            TaskList.Add(3, "Go to the grocery store");
+            TaskList.Add(4, "Fix yourself some dinner");
+            ViewBag.DataSource = TaskList;
+            return View();
+        }
+    }
+}
